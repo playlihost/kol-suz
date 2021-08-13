45e7e966136721bdaff6f6e60f824367a80a6247
Glitch (discordjsv12basitaltyapi) <none>
2021-08-13 10:03:20 +0000

    📼🎟 Checkpoint ./komutlar/oyun.js:34753445/1642

--- komutlar/oyun.js
+++ komutlar/oyun.js
@@ -1,35 +1,12 @@
 const Discord = require('discord.js');
 
-const oyunlar = [                  'Bu kesin.',
-                                    'Kesinlikle öyle.',
-                                    'Kuşkusuz.',
-                                    'Evet, Kesinlikle.',
-                                    'Buna güvenebilirsin.',
-                                    'Gördüğüm kadarıyla, Evet.',
-                                    'Büyük olasılıkla.',
-                                    'Görünüşe göre, iyi.',
-                                    'Evet.',
-                                    'İşaretler eveti gösteriyor.',
-                                    'Anlayamadım, tekrar edin.',
-                                    'Daha sonra sor.',
-                                    'Şimdi söylemesen iyi olur.',
-                                    'Tahmin edemiyorum...',
-                                    'Konsantre ol ve tekrar sor.',
-                                    'Buna güvenme.',
-                                    'Cevabım, hayır.',
-                                    'Kaynaklarım hayır diyor.',
-                                    'Görünüşe göre, bu iyi değil.',
-                                    'Çok şüpheli.',
-                                    'Şüpheli.',
-                                    'Büyük olasılıkla, hayır.',
-                                    'İçgüdüm, hayır diyor.',
-                                    'Kararsız kaldım, bidaha sormaya ne dersin?'                         
+const oyunlar = [                  'https://spotted-relieved-board.glitch.me/',                        
 ];
 
 exports.run = function(client, message, args) {
     var oyun = args.join(' ');
 
-    var oyun = cevaplar[Math.floor(Math.random() * oyunlar.length)];
+    var oyun = oyunlar[Math.floor(Math.random() * oyunlar.length)];
 
     if(!oyun) return message.reply('Bana bir oyun sormalısın! **Örnek**: >oyun <oyun>')
     else message.channel.send(oyun)
